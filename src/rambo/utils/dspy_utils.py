23eaa9542460514715afc44c927621631963c255
Magdalena Lederbauer <mlederbauer@ethz.ch>
2024-03-28 01:06:12 +0100

    feat: add retrieval model initialization

--- src/rambo/utils/dspy_utils.py
+++ src/rambo/utils/dspy_utils.py
@@ -4,39 +4,41 @@ import dspy
 import openai
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from dspy.retrieve.chromadb_rm import ChromadbRM
+
+from rambo.tools.retrieval import get_embedding_function, get_dummy_retriever
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH")
 
 
 def init_dspy(
-    database_name: str = "chroma_db",
+    database_name: str = "text-embedding-3-small_text_description",
     language_model_class=dspy.OpenAI,
     max_tokens: int = 500,
     model: str = "gpt-3.5-turbo-instruct",
+    retrieval_type: str = "embedding"
 ):
     language_model = language_model_class(max_tokens=max_tokens, model=model)
 
-    class Text(BaseModel):
-        text: str
-        long_text: str
+    if retrieval_type == "embedding":
+        embedding_function = get_embedding_function()
+        retrieval_model = ChromadbRM(
+            collection_name=database_name,
+            persist_directory=CHROMA_DB_PATH,
+            embedding_function=embedding_function,
+        )
 
-    def retrieve(query: str, k: int, **kwargs):
+    elif retrieval_type == "test":
+        retrieval_model = get_dummy_retriever().retrieve
 
-        # TODO: Implement a proper retrieval mechanism
-        # Here I return text with some information but this should be retrieved from DB.
-        # This is to test that RAG system can separate what is "good" and "relevant" from what is not.
+    elif retrieval_type == "agent":
+        # TODO @ Andres: Implement agent retrieval, maybe you need to edit the return statement?
+        # rn we pass a model as the rm param in the dspy settings
+        pass
 
-        # TODO: @Anna pls replace by proper retrieval mechanism
-        opts = [
-            "Suzuki couplings are typically performed in the presence of a palladium catalyst --typically Pd(OH)2--, a base, and a solvent like 1,4-dioxane. The reaction is typically carried out at a temperature of 83.91 degrees Celsius.",
-            "The Heck reaction is typically performed in the presence of a palladium catalyst, a base, and a solvent. The reaction is typically carried out at a temperature of 80-100 degrees Celsius.",
-            "A strange suzuki coupling was performed in the presence of a palladium catalyst, a base, and a solvent which can be toluene or THF. The reaction is typically carried out at a temperature of 6.230 degrees Celsius.",
-            "102.5 degrees Celsius is the optimal temperature for a suzuki coupling reaction, specially with water as solvent.",
-            "THF is typically used for suzuki compulings but we found that using this has very low yields.",
-            "The Sonogashira reaction is typically performed in the presence of a palladium catalyst, a base, and a solvent. The reaction is typically carried out at a temperature of 80-100 degrees Celsius.",
-            "The Buchwald-Hartwig amination is typically performed in the presence of a palladium catalyst, a base, and a solvent. The reaction is typically carried out at a temperature of 80-100 degrees Celsius.",
-        ]
-        return [Text(text=opts[i], long_text=opts[i]) for i in range(k)]
+    else:
+        raise ValueError(f"Unknown retrieval type: {retrieval_type}, choose from 'embedding', 'test', 'agent'")
 
-    dspy.settings.configure(lm=language_model, rm=retrieve)
+    dspy.settings.configure(lm=language_model, rm=retrieval_model)
